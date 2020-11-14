b99c74f297331a95801c4995c1766dd4b05b878f
jeremiegrodziski <jeremie@grodziski.com>
2020-11-14 13:57:50 +0100

    Add in memory bus

---
+++ src/kafka-bus/memory_bus.clj
@@ -0,0 +1,71 @@
+(ns kafka-bus.memory-bus
+  (:require [manifold.stream :as s :refer [consume]]
+            [manifold.bus :as b]
+            [kafka-bus.log :as log]
+            [kafka-bus.domain :refer [EventBus
+                                      publish!
+                                      subscribe]]))
+
+(def DEFAULT_STREAM_BUFFER 2048)
+
+(defrecord InMemoryEventBus [name key-extract-fn filter-fn bus]
+  EventBus
+  (publish! [_ event]
+    (if (filter-fn event)
+      (let [k (key-extract-fn event)]
+        (b/publish! bus k event)
+        (log/debug {:service ::publish :key k :event event :bus-name name} "Event published through InMemoryEventBus"))
+      (log/debug {:service ::publish :event event :bus-name name} "Discard event of InMemoryEventBus")))
+  (subscribe [_ f]
+    (throw (UnsupportedOperationException. "Can't subscribe without a key to in-memory bus")))
+  (subscribe [_ k f]
+    (let [sub-stream (b/subscribe bus k) ;; subscribing to a bus returns a manifold stream
+          exhausted? (s/consume f sub-stream)] ;; consuming a stream with a function returns a deferred
+      (log/info {:service ::subscribe :key k :bus-name name} (format "Subscribed to events with key %s through InMemoryEventBus %s" k name))
+      {:stream sub-stream
+       :exhausted? exhausted?
+       :stop (fn []
+               (s/close! sub-stream))})))
+
+(comment 
+  (defn fn-name [f]
+    (as-> (str f) $
+      (clojure.main/demunge $)
+      (or (re-find #"(.+)--\d+@" $)
+          (re-find #"(.+)@" $))
+      (last $))))
+
+(defn in-memory-event-bus
+  ([key-extract-fn]
+   (in-memory-event-bus "InMemoryBus" key-extract-fn))
+  ([name key-extract-fn]
+   (in-memory-event-bus name key-extract-fn identity))
+  ([name key-extract-fn filter-fn]
+   (in-memory-event-bus name key-extract-fn filter-fn DEFAULT_STREAM_BUFFER))
+  ([name key-extract-fn filter-fn buffer-size]
+   (log/info {:service ::in-memory-event-bus :bus-name name :buffer-size buffer-size} (format "Create InMemoryEventBus %s" name))
+   (let [stream-generator-fn #(s/stream buffer-size) ;; subscribing to an event-bus returns the stream created by this function
+         bus (->InMemoryEventBus name
+                                 key-extract-fn ;; manifold buses use the concept of topics. this function will dispatch to the correct topic
+                                 filter-fn
+                                 (b/event-bus stream-generator-fn))]
+     (log/info {:service ::in-memory-event-bus :bus-name name} (format "InMemoryEventBus %s created" name))
+     bus)))
+
+(comment
+  ;;ManifoldBus is used to wrap the event bus with the same interface as the manifold/bus
+  ;;particularly to return stream instead of invoking a callback function
+  (defprotocol ManifoldBus
+    (publish! [bus topic message])
+    (subscribe [bus topic])
+    (topic->subscribers [bus])
+    (downstream [bus topic]))
+  
+  (def source (s/stream))
+  (def filtered (s/filter even? source))
+  (s/consume prn filtered)
+  (s/put! source 1)
+  (s/put! source 2)
+  (s/put! source 3)
+  (s/put! source 4)
+  (s/put! source 6))
---
+++ test/kafka-bus/bus_test.clj
@@ -0,0 +1,61 @@
+(ns electre.common.infra.bus-test
+  (:require  [clojure.test :as t :refer [deftest testing]]
+             [clojure.spec.alpha :as s]
+             [clojure.spec.gen.alpha :as gen]
+             [clojure.test :refer [deftest assert-expr is do-report testing]]
+             [jackdaw.streams :as js]
+             [manifold.stream :as manifold]
+
+             [kafka-bus.log :refer [info]]
+             [kafka-bus.multicast-bus :refer [multicast-event-bus connect]]
+             [kafka-bus.domain :refer [defevent subscribe publish!]]
+             [kafka-bus.kafka :as k]
+             [kafka-bus.async-test-utils :refer :all]
+             [kafka-bus.kafka-bus :refer [kafka-event-bus]]
+             [kafka-bus.memory-bus :refer [in-memory-event-bus]]
+             ))
+
+(def app-config {"application.id"            "common-infra-kafka-test"
+                 "bootstrap.servers"         "localhost:9092"
+                 "cache.max.bytes.buffering" "0"})
+
+(defn consumer-config [name]
+  (assoc app-config "application.id" name))
+
+(s/def ::org-id string?) ;(s/and string? #(<= (count %) 16) not-empty))
+(s/def ::org-name string?)
+(s/def ::logo-url uri?)
+(s/def ::updated-by string?)
+(s/def ::my-test-entity (s/keys :req-un [::org-id ::org-name ::updated-by]))
+(s/def ::event-type #{::my-test-entity-created})
+(defevent ::my-test-entity-created ::my-test-entity)
+
+(defn gen-event [org-id]
+  (assoc (gen/generate (s/gen ::my-test-entity-created)) :org-id org-id))
+
+(deftest ^:integration test-multicast-kafka-in-memory-bus
+  (testing "Send events through multicast should be received in each bus connected to the multicast one"
+    (let [multicast (multicast-event-bus)
+          kafka (kafka-event-bus "Kafka event bus" app-config (k/topic-config "topic-1") (consumer-config "consumer-1") (map k/topic-config ["topic-1"]) :org-id)
+          in-memory-1 (in-memory-event-bus "in memory 1" :org-id)
+          in-memory-2 (in-memory-event-bus "in memory 2" :org-id)
+          collector-kafka (atom [])
+          collector-in-1 (atom [])
+          collector-in-2 (atom [])
+          streams-1 (subscribe kafka                 (fn [x] (info {:key (:key x)} (str "Got event from " (:name kafka)))         (swap! collector-kafka   conj x)))
+          streams-2 (subscribe in-memory-1 "leclerc" (fn [x] (info {:key (:key x)} (str "Got event from " (:name in-memory-1)))   (swap! collector-in-1    conj x)))
+          streams-3 (subscribe in-memory-2 "leclerc" (fn [x] (info {:key (:key x)} (str "Got event from " (:name in-memory-2)))   (swap! collector-in-2    conj x)))]
+      (connect multicast kafka)
+      (connect multicast in-memory-1)
+      (connect multicast in-memory-2)
+
+      (publish! multicast (gen-event "leclerc") )
+      (publish! multicast (gen-event "leclerc") )
+      (publish! multicast (gen-event "leclerc") )
+      (publish! multicast (gen-event "nimp") );only kafka should receive this one
+
+      (is (eventually (<= 3 (count @collector-in-1)) :max-elapsed 500))
+      (is (eventually (<= 3 (count @collector-in-2)) :max-elapsed 500))
+      (is (eventually (<= 4 (count @collector-kafka)) :max-elapsed 10000))
+      (js/close streams-1)
+      )))
